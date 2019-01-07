7f219b1d8198f27ca00759fd009717b6014f9b71
tania <strekasia@tut.by>
2019-01-07 14:36:54 +0300

    feat: add auth

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,18 +3,21 @@ import {Http, Response} from '@angular/http';
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
 import {map} from "rxjs/operators";
+import {AuthService} from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {
+    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {
     }
 
     storeRecipes() {
-        return this.http.put('https://recipebook-d6230.firebaseio.com/recipes.json', this.recipeService.getRecipies());
+        const token = this.authService.getToken();
+        return this.http.put('https://recipebook-d6230.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipies());
     }
 
     fetchRecipes() {
-        return this.http.get('https://recipebook-d6230.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+        this.http.get('https://recipebook-d6230.firebaseio.com/recipes.json?auth=' + token)
             .pipe(map(
                 (response: Response) => {
                     const recipes: Recipe[] = response.json();
