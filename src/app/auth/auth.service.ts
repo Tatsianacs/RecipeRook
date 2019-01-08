c6bed09148e18a094430f99a5d2d027c1a559243
Tatsianacs <42011535+Tatsianacs@users.noreply.github.com>
2019-01-08 12:31:08 +0300

    feat: ngIf for login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,35 +1,40 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
-    token: string;
-    signupUser(email: string, password: string) {
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .catch(
-                error => console.log(error)
-            )
-    }
+  token: string;
 
-    signinUser(email: string, password: string) {
-        firebase.auth().signInWithEmailAndPassword(email, password)
+  signupUser(email: string, password: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(
+        error => console.log(error)
+      );
+  }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => {
+          firebase.auth().currentUser.getIdToken()
             .then(
-                response => {
-                    firebase.auth().currentUser.getIdToken()
-                        .then(
-                            (token: string) => this.token = token
-                        )
-                }
-            )
-            .catch(
-                error => console.log(error)
+              (token: string) => this.token = token
             );
+        }
+      )
+      .catch(
+        error => console.log(error)
+      );
 
-    }
+  }
 
-    getToken() {
-       firebase.auth().currentUser.getIdToken()
-            .then(
-                (token: string) => this.token = token
-            );
-        return this.token;
-    }
+  getToken() {
+    firebase.auth().currentUser.getIdToken()
+      .then(
+        (token: string) => this.token = token
+      );
+    return this.token;
+  }
+
+  isAuthenticated() {
+    return this.token != null;
+  }
 }
