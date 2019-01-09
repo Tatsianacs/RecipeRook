077b445fbb339957dfe77cc74a493a99089743fb
Tatsianacs <42011535+Tatsianacs@users.noreply.github.com>
2019-01-09 14:50:50 +0300

    refactor: change services

--- src/app/app-routing-model.ts
+++ src/app/app-routing-model.ts
@@ -1,23 +1,26 @@
-import {RouterModule, Routes} from "@angular/router";
-import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {RecipesComponent} from "./recipes/recipes.component";
-import {NgModule} from "@angular/core";
-import {RecipeItemComponent} from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
-import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
-import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
-import {SignupComponent} from "./auth/signup/signup.component";
-import {SigninComponent} from "./auth/signin/signin.component";
-import {AuthGuard} from "./auth/auth-guard.service";
+import {RouterModule, Routes} from '@angular/router';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {NgModule} from '@angular/core';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
+import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {SigninComponent} from './auth/signin/signin.component';
+import {AuthGuard} from './auth/auth-guard.service';
+import {HomeComponent} from './core/home/home.component';
 
 const appRoutes: Routes = [
-    {path: '', redirectTo:'/recipes', pathMatch: 'full'},
-    {path: 'shopping-list', component: ShoppingListComponent}
+  {path: '', component: HomeComponent},
+  {path: 'recipes', loadChildren: './recipes/recipe.module#RecipeModule'},
+  // {path: '', redirectTo:'/recipes', pathMatch: 'full'},
+  {path: 'shopping-list', component: ShoppingListComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
 
 export class AppRoutingModel {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,35 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {HeaderComponent} from "./header/header.component";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
-import {AppRoutingModel} from "./app-routing-model";
-import {RecipeService} from "./recipes/recipe.service";
-import {HttpModule} from "@angular/http";
-import {DataStorageService} from "./shared/data-storage.service";
-import {AuthService} from "./auth/auth.service";
-import {AuthGuard} from "./auth/auth-guard.service";
-import {RecipeModule} from "./recipes/recipe.module";
-import {SharedModule} from "./shared/shared.module";
-import {ShoppingListModule} from "./shopping-list/shopping-list.module";
-import {AuthModule} from "./auth/auth.module";
+import {ReactiveFormsModule} from '@angular/forms';
+import {ShoppingListService} from './shopping-list/shopping-list.service';
+import {AppRoutingModel} from './app-routing-model';
+import {RecipeService} from './recipes/recipe.service';
+import {HttpModule} from '@angular/http';
+import {DataStorageService} from './shared/data-storage.service';
+import {AuthService} from './auth/auth.service';
+import {AuthGuard} from './auth/auth-guard.service';
+import {SharedModule} from './shared/shared.module';
+import {ShoppingListModule} from './shopping-list/shopping-list.module';
+import {AuthModule} from './auth/auth.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HeaderComponent
-    ],
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        RecipeModule,
-        ShoppingListModule,
-        AppRoutingModel,
-        HttpModule,
-        SharedModule,
-        AuthModule
-    ],
-    providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    ShoppingListModule,
+    AppRoutingModel,
+    HttpModule,
+    SharedModule,
+    AuthModule,
+    CoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
