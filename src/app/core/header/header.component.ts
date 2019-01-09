1b7a41bbe4576e0b21b2831e049895430e425515
Tatsianacs <42011535+Tatsianacs@users.noreply.github.com>
2019-01-09 15:07:42 +0300

    refactor: optimization

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,30 +1,34 @@
-import {Component, EventEmitter, Output} from "@angular/core";
-import {DataStorageService} from "../../shared/data-storage.service";
-import { Response } from '@angular/http';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {DataStorageService} from '../../shared/data-storage.service';
+import {Response} from '@angular/http';
 import {AuthService} from '../../auth/auth.service';
 
 
 @Component({
-    selector: 'app-header',
-    templateUrl:'./header.component.html',
-    styleUrls:['./header.component.css']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
-    constructor(private dataStorageService: DataStorageService, private authService: AuthService) {
-    }
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {
+  }
 
-    onSaveData() {
-        this.dataStorageService.storeRecipes()
-            .subscribe((response: Response) => console.log(response)); //subscribe here if we want to handle response in component e.g. to handle errors
-    }
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe((response: Response) => console.log(response)); //subscribe here if we want to handle response in component e.g. to handle errors
+  }
 
-    onFetchData() {
-        this.dataStorageService.fetchRecipes();
-        console.log('button is clicked');
-    }
+  onFetchData() {
+    this.dataStorageService.fetchRecipes();
+    console.log('button is clicked');
+  }
 
-    onLogout() {
-        this.authService.logout();
-    }
+  onLogout() {
+    this.authService.logout();
+  }
+
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
 }
